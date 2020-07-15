fc598023065c5063453a15dfbd5817af24049afb
Firelfy <Firelfy@users.noreply.github.com>
2020-07-15 20:53:40 +1200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if [ ! -f JMusicBot-0.2.10-Linux.jar ]
 then
 	wget https://github.com/jagrosh/MusicBot/releases/download/0.2.10/JMusicBot-0.2.10-Linux.jar;
 fi
-java -server -jar JMusicBot-${VAR}-All.jar nogui;
+java -server -jar JMusicBot-0.2.10-Linux.jar nogui;
